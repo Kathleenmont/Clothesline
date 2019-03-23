4af03c9f5e5c30c4b5b5e29ff75c0cf29043e74b
Zach Hollingsworth <zahollingsworth@Zachs-MacBook.local>
2019-03-23 14:12:59 -0400

    js update

---
+++ assets/javascript/code.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+    $('#etsy-search').bind('submit', function() {
+        api_key = "vy2ijcoryhesrk8tkbsf6geh";
+        terms = "lemon " + $('#etsy-terms').val();
+        etsyURL = "https://openapi.etsy.com/v2/listings/active.js?keywords="+
+            terms+"&limit=12&includes=Images:1&api_key="+api_key;
+
+        $('#etsy-images').empty();
+        $('<p></p>').text('Searching for '+terms).appendTo('#etsy-images');
+
+        $.ajax({
+            url: etsyURL,
+            dataType: 'jsonp',
+            success: function(data) {
+                console.log(data)
+                if (data.ok) {
+                    $('#etsy-images').empty();
+                    if (data.count > 0) {
+                        $.each(data.results, function(i,item) {
+                            $("<img/>").attr("src", item.Images[0].url_75x75).appendTo("#etsy-images").wrap(
+                                "<a href='" + item.url + "'></a>"
+                            );
+                            if (i%4 == 3) {
+                                $('<br/>').appendTo('#etsy-images');
+                            }
+                        });
+                    } else {
+                        $('<p>No results.</p>').appendTo('#etsy-images');
+                    }
+                } else {
+                    $('#etsy-images').empty();
+                    alert(data.error);
+                }
+            }
+        });
+
+        return false;
+    })
+});
