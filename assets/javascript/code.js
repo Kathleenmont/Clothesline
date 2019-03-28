5473f25ac02127336ca14f67e14bb9e717fde2cc
Zach Hollingsworth <zahollingsworth@Zachs-MacBook.local>
2019-03-27 21:32:11 -0400

    esty div

--- assets/javascript/code.js
+++ assets/javascript/code.js
@@ -1,12 +1,14 @@
 function displayClothes() {
     
+   
+
     api_key = "vy2ijcoryhesrk8tkbsf6geh";
     //terms = $('#etsy-terms').val();
     terms = $(this).attr("data-clothing");
    
     console.log(terms)
     etsyURL = "https://openapi.etsy.com/v2/listings/active.js?keywords=" + 
-        terms + "&limit=8&includes=Images:1&api_key=" + api_key;
+        terms + "&limit=12&includes=Images:1&api_key=" + api_key;
     
     // $('#etsy-images').empty();
     // $('<p></p>').text('Searching for ' + terms).appendTo('#etsy-images');
@@ -14,32 +16,39 @@ function displayClothes() {
     //var images = $("<div id=" + j + ">");
     //$("#" + terms).append(images);
     console.log("banana")
-    var breaksss = $("<div id='" + terms + "'></div>")
-    $("#" + terms + "1").append(breaksss)
+    $('#etsy-images').empty();
+    $('<p></p>').text('Searching for '+terms).appendTo('#etsy-images');
+
     $.ajax({
         url: etsyURL,
         dataType: 'jsonp',
         success: function (data) {
             console.log(data)
             if (data.ok) {
-                $("#" + terms).empty();
+                $('#etsy-images').empty();
                 if (data.count > 0) {
-                    $.each(data.results, function (i, item) {
-                        $("<img/>").attr("src", item.Images[0].url_170x135).appendTo("#" + terms).wrap(
-                            "<a href='" + item.url + "'target=_blank></a>"
+                    $.each(data.results, function(i,item) {
+                        $("<img/>").attr("src", item.Images[0].url_75x75).appendTo("#etsy-images").wrap(
+                            "<a href='" + item.url + "'></a>"
                         );
-                        
-                        //if (i % 4 == 3) {
-                        //    $('<br/>').appendTo(breaksss);
-                        //}
-                        });
-                    } else {
-                        $('<p>No results.</p>').appendTo(breaksss);
+                        if (i%4 == 3) {
+                            $('<br/>').appendTo('#etsy-images');
+                        }
+                    });
+                } else {
+                    $('<p>No results.</p>').appendTo('#etsy-images');
                 }
+            } else {
+                $('#etsy-images').empty();
+                alert(data.error);
+            }
+                    
             } 
-        } 
-    });
-}
+        });
+    }
+    
+   
+
 
 $(document).on("click", ".clothing-image", displayClothes);
 
